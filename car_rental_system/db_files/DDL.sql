bd356e667a076e5f46cc1ee136ef4fa76554c96f
Menna Magdy <mennamagdyx@gmail.com>
2024-12-20 20:53:22 +0200

    minii customer modification

--- car_rental_system/db_files/DDL.sql
+++ car_rental_system/db_files/DDL.sql
@@ -22,9 +22,9 @@ CREATE TABLE admin (
 
 CREATE TABLE customer(
     customer_id INT AUTO_INCREMENT,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR(255),
     address VARCHAR(255),
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
