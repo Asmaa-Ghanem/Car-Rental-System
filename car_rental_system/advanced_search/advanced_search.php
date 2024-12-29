fd8013ceaddd42f4418ff585d805cdc83657ab94
Marwan Kenawy <mrawanmahmoud2@gmail.com>
2024-12-29 22:42:15 +0200

    Edited advanced search columns

--- car_rental_system/advanced_search/advanced_search.php
+++ car_rental_system/advanced_search/advanced_search.php
@@ -9,10 +9,12 @@ $query = "
     SELECT 
         car.car_id, car.model AS car_model, car.year AS car_year,car.size AS car_size, car.color AS car_color, car.plate_id AS car_plate_id, car.status AS car_status, car.price_per_day AS car_price_per_day,
         customer.name AS customer_name, customer.email AS customer_email, customer.phone AS customer_phone, customer.address AS customer_address,
-        reservation.start_date AS reservation_start, reservation.end_date AS reservation_end, reservation.payment_amount AS reservation_payment_amount
+        reservation.start_date AS reservation_start, reservation.end_date AS reservation_end, p.pickup_id, p.pickup_date, p.pickup_location_id, re.return_id, re.return_date, re.return_location_id
     FROM reservation
     INNER JOIN car ON reservation.car_id = car.car_id
     INNER JOIN customer ON reservation.customer_id = customer.customer_id
+    INNER JOIN pickup p ON reservation.reservation_id = p.reservation_id
+    INNER JOIN `return` re ON reservation.reservation_id = re.reservation_id
     WHERE 
         car.car_id LIKE ? OR
         car.model LIKE ? OR
@@ -26,11 +28,16 @@ $query = "
         customer.address LIKE ? OR
         reservation.start_date LIKE ? OR
         reservation.end_date LIKE ? OR
-        reservation.payment_amount LIKE ?
+        p.pickup_id LIKE ? OR
+        p.pickup_date LIKE ? OR
+        p.pickup_location_id LIKE ? OR
+        re.return_id LIKE ? OR
+        re.return_date LIKE ? OR
+        re.return_location_id LIKE ?
 ";
 
 $stmt = $conn->prepare($query);
-$stmt->bind_param('sssssssssssss',$search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search);
+$stmt->bind_param('ssssssssssssssssss',$search,$search,$search,$search,$search,$search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search, $search);
 $stmt->execute();
 $result = $stmt->get_result();
 
